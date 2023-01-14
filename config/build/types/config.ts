e90f475da3179e81d37c6e6548e5a9a40e3a7341
A.Kashuta <a@artka.dev>
2023-01-14 12:10:03 +0300

    webpack config decompose

---
+++ config/build/types/config.ts
@@ -0,0 +1,12 @@
+
+export type BuildMode = 'development' | 'production' | 'none';
+export type BuildPaths = {
+   entry: string;
+   build: string;
+   html: string;
+}
+export interface BuildOptions {
+    mode:BuildMode;
+    paths:BuildPaths;
+    isDevServer?:boolean;
+}
